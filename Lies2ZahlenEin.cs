7e51f32e5777d75b4f251bddea10c96164f47404
Sim-ctrl <77060991+Sim-ctrl@users.noreply.github.com>
2024-03-01 09:21:55 +0100

    Aufgabe 1
    Lies 2 Zahlen von der Konsole ein. Wenn die erste Zahl größer ist, gib die erste Zahl auf der Konsole aus. Falls die 2 Zahl größer oder gleich groß ist, gibt ihren Wert aus.

---
+++ Lies2ZahlenEin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.IO;
+
+
+    
+        Console.WriteLine("Erste Zahl:");
+        int zahl1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Zweite Zahl:");
+        int zahl2 = Convert.ToInt32(Console.ReadLine());
+
+        if (zahl1 > zahl2)
+        {
+            Console.WriteLine("Die erste Zahl ist größer: " + zahl1);
+        }
+        else
+        {
+            Console.WriteLine("Die zweite Zahl ist größer oder gleich groß: " + zahl2);
+        }
+
+//this.Close();
+System.Environment.Exit(1);
+
+
+
