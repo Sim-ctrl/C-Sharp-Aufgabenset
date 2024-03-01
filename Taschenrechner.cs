482b7a9f07c2d79b6acfb98c1093ae9b040504c9
Sim-ctrl <77060991+Sim-ctrl@users.noreply.github.com>
2024-03-01 09:27:04 +0100

    Aufgabe 6
    Erstelle einen Taschenrechner, der auf der Konsole arbeitet. Dieser Taschenrechner soll Mathematische Zeichen im Text erkennen und die Zahlen auf beiden Seiten verrechnen.

---
+++ Taschenrechner.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Willkommen im Taschenrechner. Gib deine Rechnung an");
+double loesung = 0;
+string s1 = Convert.ToString(Console.ReadLine());
+
+//string helloWorld = "Hello, world!";
+//string s1 = "The quick brown fox jumps over the lazy dog";
+string plus = "+";
+bool bplus = s1.Contains(plus);
+string minus = "-";
+bool bminus = s1.Contains(minus);
+string mal = "*";
+bool bmal = s1.Contains(mal);
+string gete = "/";
+bool bgete = s1.Contains(gete);
+
+if (bgete) {
+    string[] subs = s1.Split('/');
+    double[] subsDouble = new double[subs.Length];
+    for (int i = 0; i < subs.Length; i++)
+    {
+        subsDouble[i] = Convert.ToDouble(subs[i]);
+    }
+    for (int i = 0; i < subsDouble.Length - 1; i++)
+    {
+        loesung = subsDouble[i] / subsDouble[i + 1];
+    }
+}
+if (bmal) {
+    string[] subs = s1.Split('*');
+    double[] subsDouble = new double[subs.Length];
+    for (int i = 0; i < subs.Length; i++)
+    {
+        subsDouble[i] = Convert.ToDouble(subs[i]);
+    }
+    for (int i = 0; i < subsDouble.Length - 1; i++)
+    {
+        loesung = subsDouble[i] * subsDouble[i + 1];
+    }
+}   
+if  (bminus) {
+    string[] subs = s1.Split('-');
+    double[] subsDouble = new double[subs.Length];
+    for (int i = 0; i < subs.Length; i++)
+    {
+        subsDouble[i] = Convert.ToDouble(subs[i]);
+    }
+    for (int i = 0; i < subsDouble.Length-1; i++)
+    {
+        loesung = subsDouble[i] - subsDouble[i+1];
+    }
+}
+if (bplus) {
+    string[] subs = s1.Split('+');
+    double[] subsDouble = new double[subs.Length];
+    for (int i = 0; i < subs.Length; i++)
+    {
+        subsDouble[i] = Convert.ToDouble(subs[i]);
+    }
+    for (int i = 0; i < subsDouble.Length; i++)
+    {
+        loesung = subsDouble[i] + loesung;
+    }
+}
+
+Console.WriteLine("Die Lösung lautet " + loesung);
+Console.WriteLine(" ");
+Console.WriteLine("");
