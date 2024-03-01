9747361350bb9c66c2c08fb96d90bf9f0dcc5509
Sim-ctrl <77060991+Sim-ctrl@users.noreply.github.com>
2024-03-01 09:33:42 +0100

    Aufgabe 10
    Monty Hall war ein Moderator der Spielshow Let's Make a Deal im deutschen Geh auf's Ganze! bei dem Kandidaten ein Auto gewinnen konnten das hinter einem von drei Toren stand. Der Kandidat konnte ein Tor auswählen und danach wurde eines der Tore geöffnet, hinter denen ein Spottpreis stand (in der Problemstellung üblicherweise eine Ziege). Der Kandidat hatte dann noch einmal die Gelegenheit das verbleibende Tor zu wählen oder bei seiner ersten Wahl zu bleiben. Welche der beiden Möglichkeiten sollte der Kandidat nehmen? Auf seiner ersten Wahl bleiben oder wechseln? 
     
     
    Das Programm soll das Spiel 200-mal durchgehen, jeweils 100-mal je Wahlmöglichkeit des Kandidaten. Welche der Möglichkeiten hat wie oft zum Erfolg geführt?

---
+++ Ziegenproblem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Ziegenproblem
+{
+    static void Main(string[] args)
+    {
+        int anzahlDurchgänge = 200;
+        int erfolgeBleiben = 0;
+        int erfolgeWechseln = 0;
+
+        Random zufall = new Random();
+
+        for (int i = 0; i < anzahlDurchgänge; i++)
+        {
+            int gewinnTür = zufall.Next(3); // Zufällige Auswahl der Tür mit dem Gewinn
+
+            int ersteWahl = zufall.Next(3); // Kandidat wählt zuerst eine Tür
+
+            // Tür, die der Moderator öffnet (kann nicht die Gewinn-Tür oder die erste Wahl des Kandidaten sein)
+            int öffnenTür = zufall.Next(3);
+
+            while (öffnenTür == gewinnTür || öffnenTür == ersteWahl)//Damit weder die gewinnende Tür noch die bereits gewählte Tür gewählt wird.
+                {
+                    öffnenTür = zufall.Next(3);
+                } 
+
+
+
+            /////// Kandidat bleibt
+            if (ersteWahl == gewinnTür)
+            {
+                erfolgeBleiben++;
+            }
+
+
+
+            ///////////////// Kandidat wechseltt
+            int neueWahl=zufall.Next(3);
+            
+            while (neueWahl == ersteWahl || neueWahl == öffnenTür){ // Damit wählt er die andere Seite aus
+                neueWahl = zufall.Next(3);
+            } 
+
+            if (neueWahl == gewinnTür)
+            {
+                erfolgeWechseln++;
+            }
+        }
+
+        Console.WriteLine("Erfolge beim Bleiben: " + erfolgeBleiben);    // eine 33% Wahl
+        Console.WriteLine("Erfolge beim Wechseln: " + erfolgeWechseln); // eine 50% Wahl
+
+        string s = Convert.ToString(Console.ReadLine());
+    }
+}
