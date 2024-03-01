b7e52859b4d78538fe1bb6292b0dc617df8ed592
Sim-ctrl <77060991+Sim-ctrl@users.noreply.github.com>
2024-03-01 09:26:23 +0100

    Aufgabe 5
    Das Sieb des Eratosthenes ist ein einfacher Weg Primzahlen zu finden. 
    Die Zahlen werden nebeneinander aufgelistet und die Zahlen, die ein vielfaches einer anderen Zahl sind gestrichen. Am Ende bleiben nur noch Primzahlen übrig.
     
    2,3,4,5,6,7,8,9,10,11,12,13,14,15…
    2,3,4,5,6,7,8,9,10,11,12,13,14,15… Jede zweite Zahl nach 2 wird gestrichen
    2,3,4,5,6,7,8,9,10,11,12,13,14,15… Jede dritte Zahl nach 3 wird gestrichen
    2,3,4,5,6,7,8,9,10,11,12,13,14,15… Jede fünfte Zahl nach 5 wird gestrichen
     
    Um Zahlen streichen zu können, kann hier nicht der Datentyp int verwendet werden, sondern es muss eine eigene Klasse erstellt werden. 
     
    public class Siebzahl
    Public Wert as Integer 
    Public Gestrichen as boolean  =  false
    End Class
     
    Erstelle ein Array und fülle es mit den Zahlen von Zwei bis Hundert. Gib nach dem Streichen der Zahlen alle nicht gestrichenen Zahlen auf der Konsole aus.

---
+++ Eratosthenes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+Console.WriteLine("Hello, World!");
+int[] students = new int[98];
+
+
+for (int i = 2; i < 100; i++)
+{
+    students[(i-2)] = i;
+}
+
+
+for (int j = 0; j < students.Length; j++)
+{ Console.Write(students[j]+" ");
+}
+
+///////////////////////Durch 2
+Thread.Sleep(4000);
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("Jetzt berechnen wir die Reihe, wenn zudem jede durch 2 teilbare Zahl gestrichen wird "); 
+Thread.Sleep(4000);
+
+
+for (int j = 0; j < students.Length; j++) {
+
+    if (students[j]%2!=0) { Console.Write(students[j]+" "); }
+}
+
+/////////////////////////Durch 3
+Thread.Sleep(4000);
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("Jetzt berechnen wir die Reihe, wenn zudem jede durch 3 teilbare Zahl gestrichen wird ");
+Thread.Sleep(4000);
+
+
+for (int j = 0; j < students.Length; j++)
+{
+    if (students[j] % 2 != 0) { if (students[j] % 3 != 0) { Console.Write(students[j] + " "); } }
+}
+
+/////////////////////////Durch 5
+Thread.Sleep(4000);
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("Jetzt berechnen wir die Reihe, wenn zudem jede durch 5 teilbare Zahl gestrichen wird ");
+Thread.Sleep(4000);
+
+for (int j = 0; j < students.Length; j++)
+{
+    if (students[j] % 2 != 0) { if (students[j] % 3 != 0) { if (students[j] % 5 != 0) { Console.Write(students[j] + " "); } } }
+}
+
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
