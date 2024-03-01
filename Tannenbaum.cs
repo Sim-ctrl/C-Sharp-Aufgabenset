0f3fb2940f1ff703f1b920f0113302e3e3fd3ab8
Sim-ctrl <77060991+Sim-ctrl@users.noreply.github.com>
2024-03-01 09:28:15 +0100

    Aufgabe 7
    Gib auf der Konsole einen Tannenbaum aus. Du sollst aber vorher eingeben können wie groß der Tannenbaum werden soll. Der Stamm zählt extra.

---
+++ Tannenbaum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Willkommen in der Baumschule - Gib deine gewollte Höhe an");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+int hoehe = Convert.ToInt32(Console.ReadLine());
+int hoehe1 = hoehe;
+
+for (int j = 0; j < hoehe1; j++)
+{
+for (int i = 0; i < hoehe; i++) {
+    Console.Write(" ");
+}
+for (int i = 0; i < (hoehe1-hoehe); i++){
+        Console.Write("##");}
+    Console.Write("#");
+    Console.WriteLine("");
+    hoehe = hoehe - 1;
+    System.Threading.Thread.Sleep(1300);
+}
+
+//Fuer den Baumstumpf:
+for (int i = 0; i < (hoehe1-1); i++)
+{
+    Console.Write(" ");
+}
+Console.Write("|_|");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
