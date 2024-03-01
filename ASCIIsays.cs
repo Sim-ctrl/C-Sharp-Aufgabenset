a0d668fda103681e24b581836c9a6c63bf3415a0
Sim-ctrl <77060991+Sim-ctrl@users.noreply.github.com>
2024-03-01 09:32:25 +0100

    Aufgabe 11
    Es soll eine ASCII Zeichnung auf der Konsole ausgegeben werden. Die ASCII Zeichnung soll aber einen Text als Sprechblase über dem Kopf ausgeben. Je nach Länge des Textes muss auch die Sprechblase wachsen um den Text fassen zu können mehrzeilige Texte müssen in der ersten Version nicht beachtet werden. Die Übergabe welcher Text ausgegeben werden soll, soll an der Konsole eingelesen werden.

---
+++ ASCIIsays.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Aufgabe14
+{
+    internal class ASCIIsays
+    {
+        static void Main(string[] args)
+        {
+            ArrayList stringArrayList = new ArrayList();
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("WIr erstellen heute ein kleines Pferdchen, das DEINEN Text sagt. Wass soll das Pferdchen sagen?(Leer lassen, wenn kein Text angezeigt werden soll)");
+            Console.WriteLine("DU kannst auch einen Zeilenumbruch realisieren: Nutze /n dazu");
+            string s = Convert.ToString(Console.ReadLine());
+
+            if (s.Contains("\n\n"))
+            {
+                Console.WriteLine("Der eingegebene Text enthält Zeilenumbrüche. Sind Sie sicher, dass Sie fortfahren möchten? (ja/nein)");
+                string bestätigung = Console.ReadLine();
+
+                if (bestätigung.ToLower() == "nein")
+                {
+                    Environment.Exit(0);
+                }
+            }
+
+            string peter = s;
+            // peter = "";
+            string untStrichObn = "";
+            string untStrichUnt = "";
+
+            for (int i = 0; i< peter.Length; i++)
+            {
+                untStrichObn = untStrichObn + "_";
+
+            }
+            for (int i = 0; i < (peter.Length-1); i++)
+            {
+                untStrichUnt = untStrichUnt + "_";
+
+            }
+
+
+
+            string pferd = " ____"+ untStrichObn + "____\r\n/    "+peter+"    \\\r\n\\_____    "+ untStrichUnt+"/\r\n      \\  / \r\n       \\ |  ^^\r\n        \\| (oo)\\_______\r\n           (__)        )\\\r\n               ||----- |\\\\\r\n               ||     ||\r\n";
+             string pferdohne = "            ^^\r\n           (oo)\\_______\r\n           (__)        )\\\r\n               ||----- |\\\\\r\n               ||     ||\r\n";
+
+            if (peter.Length == 0) {
+                Console.WriteLine(pferdohne);
+            }
+            else { 
+            Console.WriteLine(pferd);
+        }}
+    }
+}
